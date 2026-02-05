607ad1485681a2f45966565fb3bc44a0599ebaff
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-02-05 09:34:26 -0600

    Update unit5 with code from previous units

--- docs/unit03/scripts/ml_data_analysis.py
+++ docs/unit03/scripts/ml_data_analysis.py
@@ -1,34 +1,9 @@
 import json
-from pydantic import BaseModel
+from models import MeteoriteLanding, compute_average_mass, check_hemisphere, count_classes
 
-class MeteoriteLanding(BaseModel):
-    name: str
-    id: int
-    class_name: str
-    mass: int
-    lat: float
-    long: float
-
-def compute_average_mass(landings: list[MeteoriteLanding]) -> float:
-    total_mass = 0.
-    for ml in landings:
-        total_mass += ml.mass
-    return (total_mass / len(landings))
-
-def check_hemisphere(ml: MeteoriteLanding) -> str:
-    location = ''
-    if (ml.lat > 0):
-        location = 'Northern'
-    else:
-        location = 'Southern'
-    if (ml.long > 0):
-        location = f'{location} & Eastern'
-    else:
-        location = f'{location} & Western'
-    return(location)
 
 def main():
-    with open('Meteorite_Landings_Simple.json', 'r') as f:
+    with open('Meteorite_Landings.json', 'r') as f:
         ml_data = json.load(f)
 
     landings = [MeteoriteLanding(**ml) for ml in ml_data["meteorite_landings"]]
@@ -37,6 +12,8 @@ def main():
 
     for ml in landings:
         print(check_hemisphere(ml))
-        
+
+    print(count_classes(landings))
+
 if __name__ == '__main__':
     main()
